f95137a9f544d6089bcd7b424bb1fed62f39db2a
Alan Oliveira de Sousa <alandivan@gmail.com>
2020-10-03 13:29:49 -0300

    Criação da entidade do Repository

---
+++ src/main/java/br/com/faculdade/api/repository/PessoaRepository.java
@@ -0,0 +1,10 @@
+package br.com.faculdade.api.repository;
+
+import br.com.faculdade.api.domain.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+}
