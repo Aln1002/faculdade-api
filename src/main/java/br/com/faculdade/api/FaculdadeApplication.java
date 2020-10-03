1532aa529f54a9af75b6015abf2d7fba63daeaf6
Alan Oliveira de Sousa <alandivan@gmail.com>
2020-10-03 11:09:33 -0300

    projeto inicial

---
+++ src/main/java/br/com/faculdade/api/FaculdadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.faculdade.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaculdadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaculdadeApplication.class, args);
+    }
+
+}
